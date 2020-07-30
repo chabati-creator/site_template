51179540a671afdb6d5eca7f6fa456e53985f13c
chabati-creator <61482164+chabati-creator@users.noreply.github.com>
2020-07-30 23:07:13 +1200

    Quick search bar

--- bottombit.php
+++ bottombit.php
@@ -1,8 +1,15 @@
         <div class="box side">
             
-            <h2>Add an App | <a class="side" href="showall.php">Show All</a></h2>
+            <h2>Add an App | 
+                <a class="side" href="showall.php">Show All</a></h2>
             
-            <p>This is where the search are goes</p>
+            <form class="searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
+            
+                <input class="search" type="text" name="dev_name" size="40" value="" required placeholder="App Nam / Developer Name..."/>
+                
+                <input class="submit" type="submit" name="find_dev_name" value="&#xf002;"/>
+            
+            </form>
             
             
         </div> <!-- / side bar -->
